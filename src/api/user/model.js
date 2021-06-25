afbb4670acfc6f96b5e156598bc9e3b58f15c5a4
Shivamurugan <shiva.m@indiudmsoft.com>
2021-06-25 17:37:43 +0530

    Removed user and admin constraints, added userType in user model

--- src/api/user/model.js
+++ src/api/user/model.js
@@ -4,7 +4,7 @@ import mongoose, { Schema } from 'mongoose'
 import mongooseKeywords from 'mongoose-keywords'
 import { env } from '../../config'
 
-const roles = ['user', 'admin']
+// const roles = ['user', 'admin']
 
 const userSchema = new Schema({
   email: {
@@ -152,7 +152,7 @@ userSchema.methods = {
 }
 
 userSchema.statics = {
-  roles
+  // roles
 }
 
 userSchema.plugin(mongooseKeywords, { paths: ['email', 'name'] })
