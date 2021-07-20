fbddae83f16fc3cea819106a82a333e8b44eca0c
schittemsetty <akhilendhar.g@indiumsoft.com>
2021-07-20 12:34:13 +0530

    debugging

--- src/api/taskAssignment/controller.js
+++ src/api/taskAssignment/controller.js
@@ -771,28 +771,28 @@ export const getUsers = async ({
           qaObject.primaryPillar = false;
           analystObject.primaryPillar = false;
         }
-        console.log('before qa', group.assignedMembers[index].roleDetails.primaryRole, qaId);
-        if (qaId && group.assignedMembers[index].roleDetails.primaryRole === qaId) {
+        console.log('before qa', group.assignedMembers[index].roleDetails.primaryRole, qaId._id);
+        if (qaId && group.assignedMembers[index].roleDetails.primaryRole === qaId._id) {
           console.log('in if qaId')
           qaObject.id = group.assignedMembers[index].id;
           qaObject.name = group.assignedMembers[index].name;
           qaObject.primaryRole = true;
           qa.push(qaObject);
-        } else if (qaId && group.assignedMembers[index].roleDetails.roles.indexOf(qaId) > -1) {
+        } else if (qaId && group.assignedMembers[index].roleDetails.roles.indexOf(qaId._id) > -1) {
           console.log('in else if qaId')
           qaObject.id = group.assignedMembers[index].id;
           qaObject.name = group.assignedMembers[index].name;
           qaObject.primaryRole = false;
           qa.push(qaObject);
         }
-        console.log('before analyst', group.assignedMembers[index].roleDetails.primaryRole, analystId);
-        if (analystId && group.assignedMembers[index].roleDetails.primaryRole === analystId) {
+        console.log('before analyst', group.assignedMembers[index].roleDetails.primaryRole, analystId._id);
+        if (analystId && group.assignedMembers[index].roleDetails.primaryRole === analystId._id) {
           console.log('in if anylysy')
           analystObject.id = group.assignedMembers[index].id;
           analystObject.name = group.assignedMembers[index].name;
           analystObject.primaryRole = true;
           analyst.push(analystObject);
-        } else if (analystId && group.assignedMembers[index].roleDetails.roles.indexOf(analystId) > -1) {
+        } else if (analystId && group.assignedMembers[index].roleDetails.roles.indexOf(analystId._id) > -1) {
           console.log('in else if anylysy')
           analystObject.id = group.assignedMembers[index].id;
           analystObject.name = group.assignedMembers[index].name;
