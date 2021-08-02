f0a6c6565b97daf4ce66f964074cf5697ce500bd
FIRST_NAME LAST_NAME <naveen.m@indiumsoft.com>
2021-08-02 11:22:06 +0530

    added schema for to update the validations collection

--- src/api/validations/model.js
+++ src/api/validations/model.js
@@ -1,63 +1,153 @@
 import mongoose, { Schema } from 'mongoose'
 
 const validationsSchema = new Schema({
-  createdBy: {
-    type: Schema.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  datapointId: {
-    type: Schema.ObjectId,
-    ref: 'Datapoints',
-    required: true
-  },
-  validationRule: {
-    type: String
-  },
-  rule: {
-    type: String
-  },
-  dependantCode: {
-    type: String
-  },
-  condition: {
-    type: String
-  },
-  criteria: {
-    type: String
-  },
-  validationAlert: {
-    type: String
-  },
-  status: {
-    type: Boolean,
-    default: true
-  }
+//   createdBy: {
+//     type: Schema.ObjectId,
+//     ref: 'User',
+//     required: true
+//   },
+//   datapointId: {
+//     type: Schema.ObjectId,
+//     ref: 'Datapoints',
+//     required: true
+//   },
+//   validationRule: {
+//     type: String
+//   },
+//   rule: {
+//     type: String
+//   },
+//   dependantCode: {
+//     type: String
+//   },
+//   condition: {
+//     type: String
+//   },
+//   criteria: {
+//     type: String
+//   },
+//   validationAlert: {
+//     type: String
+//   },
+//   status: {
+//     type: Boolean,
+//     default: true
+//   }
+// }, {
+//   timestamps: true,
+//   toJSON: {
+//     virtuals: true,
+//     transform: (obj, ret) => { delete ret._id }
+//   }
+// })
+
+// validationsSchema.methods = {
+//   view (full) {
+//     const view = {
+//       // simple view
+//       id: this.id,
+//       createdBy: this.createdBy ? this.createdBy.view(full) : null,
+//       datapointId: this.datapointId ? this.datapointId.view(full) : null,
+//       validationRule: this.validationRule,
+//       rule: this.rule,
+//       dependantCode: this.dependantCode,
+//       condition: this.condition,
+//       criteria: this.criteria,
+//       validationAlert: this.validationAlert,
+//       status: this.status,
+//       createdAt: this.createdAt,
+//       updatedAt: this.updatedAt
+//     }
+
+datapointId: {
+  type: Schema.ObjectId,
+  ref: 'Datapoints',
+  required: true
+},
+dpCode: {
+  type: String,
+  required: true
+},
+clientTaxonomyId: {
+  type: Schema.ObjectId,
+  ref: 'ClientTaxonomy',
+  required: true
+}, 
+validationRule: {
+  type: String
+},
+dataType: {
+  type: String,
+  required: true
+},
+hasDependentCode: {
+  type: Boolean
+},
+dependentCodes: {
+  type: Array
+},
+validationTypes: {
+  type: String
+},
+percentileThreasholdValue: {
+  type: String
+},
+parameters: {
+  type: Array
+},
+methodName: {
+  type: String
+},
+checkCondition: {
+  type: String
+},
+criteria: {
+  type: String
+},
+checkResponse: {
+  type: Array
+},
+errorMessage: {
+  type: String
+},
+status: {
+  type: Boolean,
+  default: true
+}
 }, {
-  timestamps: true,
-  toJSON: {
-    virtuals: true,
-    transform: (obj, ret) => { delete ret._id }
-  }
+timestamps: true,
+toJSON: {
+  virtuals: true,
+  transform: (obj, ret) => { delete ret._id }
+}
 })
 
 validationsSchema.methods = {
-  view (full) {
-    const view = {
-      // simple view
-      id: this.id,
-      createdBy: this.createdBy ? this.createdBy.view(full) : null,
-      datapointId: this.datapointId ? this.datapointId.view(full) : null,
-      validationRule: this.validationRule,
-      rule: this.rule,
-      dependantCode: this.dependantCode,
-      condition: this.condition,
-      criteria: this.criteria,
-      validationAlert: this.validationAlert,
-      status: this.status,
-      createdAt: this.createdAt,
-      updatedAt: this.updatedAt
-    }
+view (full) {
+  const view = {
+    // simple view
+    id: this.id,
+    datapointId: this.datapointId ? this.datapointId.view(full) : null,
+    dpCode: this.dpCode,
+    clientTaxonomyId: this.clientTaxonomyId ? this.clientTaxonomyId.view(full) : null,
+    validationRule: this.validationRule,
+    dataType: this.dataType,
+    hasDependentCode: this.hasDependentCode,
+    dependantCodes: this.dependantCodes,
+    validationTypes: this.validationTypes,
+    percentileThreasholdValue: this.percentileThreasholdValue,
+    parameters: this.parameters,
+    methodName: this.methodName,
+    checkCondition: this.checkCondition,
+    criteria: this.criteria,
+    checkResponse: this.checkResponse,
+    sucessAlert: this.sucessAlert,
+    failureAlert: this.failureAlert,
+    status: this.status,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt
+  }
+
 
     return full ? {
       ...view
