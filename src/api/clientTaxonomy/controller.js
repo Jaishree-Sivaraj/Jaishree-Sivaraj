6842e029246dc2a1b356ce633b0970d33c80d6f5
Shivamurugan <shiva.m@indiudmsoft.com>
2021-08-06 18:10:26 +0530

    Added nic logs

--- src/api/clientTaxonomy/controller.js
+++ src/api/clientTaxonomy/controller.js
@@ -60,8 +60,10 @@ export const index = async({ querymen: { query, select, cursor } }, res, next) =
               nicCodeList.push({ value: cmpItem.nic, label: cmpItem.nic });
             }
           }
+          console.log('nicCodeList', nicCodeList);
           
           let nicList = _.uniqBy(nicCodeList, 'nic');
+          console.log('nicList', nicList);
           let objectToPush = {
             _id: item.id,
             taxonomyName: item.taxonomyName,
