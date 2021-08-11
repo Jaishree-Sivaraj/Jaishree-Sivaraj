45b46bf6119c39ca511e09a870ed53747ded9d4a
Shivamurugan <shiva.m@indiudmsoft.com>
2021-08-11 15:34:24 +0530

    Added index before pushing into the additionalDetails array

--- src/api/controversy/controller.js
+++ src/api/controversy/controller.js
@@ -534,13 +534,21 @@ export const fetchDatapointControversy = async ({ params, user }, res, next) => 
                       inputType: displayFields[dIndex].inputType,
                       inputValues: optionValues.length > 0 ? optionValues : optionVal
                     })
-                    responseObject.additionalDetails.push({
+                    if (responseObject.additionalDetails.findIndex({
                       fieldName: displayFields[dIndex].fieldName,
                       name: displayFields[dIndex].name,
                       value: '',
                       inputType: displayFields[dIndex].inputType,
                       inputValues: optionValues.length > 0 ? optionValues : optionVal
-                    })
+                    }) == -1) {
+                      responseObject.additionalDetails.push({
+                        fieldName: displayFields[dIndex].fieldName,
+                        name: displayFields[dIndex].name,
+                        value: '',
+                        inputType: displayFields[dIndex].inputType,
+                        inputValues: optionValues.length > 0 ? optionValues : optionVal
+                      })                      
+                    }
                   }                
                 }
 
