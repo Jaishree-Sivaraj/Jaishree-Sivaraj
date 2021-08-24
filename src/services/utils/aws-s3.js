97aa45de07e2ee228b3e12356003120dae4f0598
schittemsetty <akhilendhar.g@indiumsoft.com>
2021-08-24 18:05:29 +0530

    utils function to store images in s3

---
+++ src/services/utils/aws-s3.js
@@ -0,0 +1,29 @@
+import * as AWS from 'aws-sdk'
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    signatureVersion: 'v4',
+    region: 'ap-south-1'
+});
+
+async function storeFileInS3(data, companyId, dataPointId, year, bucketName) {
+    return new Promise(function (resolve, reject) {
+        var fileName = `${companyId}_${year ? year + '_' : ''}${Date.now()}.json`;
+        console.log('filName', fileName);
+        const params = {
+            Bucket: process.env.BUCKET_NAME, // pass your bucket name
+            Key: type + '/' + fileName, // file will be saved in <folderName> folder
+            Body: Buffer.from(JSON.stringify(actualJson))
+        };
+        s3.upload(params, function (s3Err, data) {
+            if (s3Err) {
+                console.log('s3', s3Err);
+                reject(s3Err)
+            } else {
+                resolve({ data, fileName: type + '/' + fileName });
+            }
+        });
+    })
+}
+
+module.exports = { storeFileInS3 }
