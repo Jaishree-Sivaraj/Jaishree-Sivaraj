10be1331f475fd6e61af4620362a14ef01cd2933
Ishusan-Indium <iswariya.sankar@indiumsoft.com>
2021-08-27 12:01:29 +0530

    dataCollection

--- src/api/datapoints/controller.js
+++ src/api/datapoints/controller.js
@@ -1050,11 +1050,19 @@ export const getCategorywiseDatapoints = async (req, res, next) => {
                 datapointsObject.status = 'Completed';
               }
               if(yearfind > -1){
-                dpCodesData[yearfind].fiscalYear = dpCodesData[yearfind].fiscalYear.concat(",",errorDatapoints[errorDpIndex].year)
+                dpCodesData[yearfind].fiscalYear = dpCodesData[yearfind].fiscalYear.concat(",",errorDatapoints[errorDpIndex].year);
+                dpCodesData[yearfind].status = 'Completed';
               }else {
+                let dpCodeStatus = errorDatapoints.findIndex(obj => obj.datapointId.code == errorDatapoints[errorDpIndex].datapointId.code && obj.correctionStatus == 'Completed');
+              if(dpCodeStatus > -1){
+                datapointsObject.status = 'Completed';
+              }
                 dpCodesData.push(datapointsObject);
                 }
-            } else {
+            } else {let dpCodeStatus = errorDatapoints.findIndex(obj => obj.datapointId.code == errorDatapoints[errorDpIndex].datapointId.code && obj.correctionStatus == 'Completed');
+            if(dpCodeStatus > -1){
+              datapointsObject.status = 'Completed';
+            }
             dpCodesData.push(datapointsObject);
             }
 
