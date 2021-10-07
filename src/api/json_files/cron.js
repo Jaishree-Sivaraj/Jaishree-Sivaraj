b8243ff4a1e0099c25182285cd5d2cea81a2d38c
schittemsetty <akhilendhar.g@indiumsoft.com>
2021-10-07 15:55:57 +0530

    company task json generation removed duplicates tasks

--- src/api/json_files/cron.js
+++ src/api/json_files/cron.js
@@ -21,11 +21,6 @@ cron.schedule('0 3 * * saturday', () => {
     handleCron();
 });
 
-cron.schedule('50 18 * * tuesday', () => {
-    console.log('in cron job')
-    //handleCron();
-});
-
 async function handleCron() {
     var companiesTasks = await CompaniesTasks.find({ canGenerateJson: true, isJsonGenerated: false, status: true }).populate('categoryId').populate({
         path: 'companyId',
@@ -33,23 +28,26 @@ async function handleCron() {
             path: 'clientTaxonomyId'
         }
     });
-    //TODO
-    //loop companiesTasks here and in each iteration push the companyId and year as object into new array
-    //but before pushing into the array check if that companyId and year already exist in that array
-    //if exist don't push, so that we will have only one entry for each company and year
-    //and then instead of looping companiesTasks and calling generateJson you should loop that new array 
-    // and call generateJson from that so that instead of iterating 3 times same company and year it will iterate only one time
+    console.log('companiesTasks', companiesTasks.length);
+    var companiesTasksArr = [];
+    companiesTasks.forEach(function (rec) {
+        var obj = {};
+        obj.companyId = rec.companyId.id;
+        obj.year = rec.year;
+        if (companiesTasksArr.findIndex((rec) => rec.companyId === obj.companyId && rec.year === obj.year) === -1) {
+            companiesTasksArr.push(obj);
+        }
+    })
     var controversyTasks = await ControversyTasks.find({ canGenerateJson: true, isJsonGenerated: false, status: true }).populate({
         path: 'companyId',
         populate: {
             path: 'clientTaxonomyId'
         }
     });
-    for (let index = 0; index < companiesTasks.length; index++) {
+    for (let index = 0; index < companiesTasksArr.length; index++) {
         console.log('index', index);
         try {
-            var result = await generateJson({ type: 'data', companyId: companiesTasks[index].companyId.id, year: companiesTasks[index].year });
-            console.log('res', result);
+            var result = await generateJson({ type: 'data', companyId: companiesTasksArr[index].companyId, year: companiesTasksArr[index].year });
         } catch (e) {
             console.log('error while generating jsons for data', e);
         }
