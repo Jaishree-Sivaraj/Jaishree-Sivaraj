561129dae95863d13b24a61a32f33c4e1e82d6ec
Naveen Mannem <naveen.m@indiumsoft.com>
2021-11-10 13:48:18 +0530

    Taxonomy while downloading polarity and relevant for India issue fixed

--- src/api/datapoints/controller.js
+++ src/api/datapoints/controller.js
@@ -4708,10 +4708,6 @@ export const uploadNewTaxonomyDatapoints = async (req, res, next) => {
               for (let unqCatIndex = 0; unqCatIndex < newDatapoints.length; unqCatIndex++) {
                 await Datapoints.updateOne({
                   clientTaxonomyId: req.body.clientTaxonomyId,
-                  categoryId: newDatapoints[unqCatIndex].categoryId,
-                  themeId: newDatapoints[unqCatIndex].themeId,
-                  keyIssueId: newDatapoints[unqCatIndex].keyIssueId,
-                  functionId: newDatapoints[unqCatIndex].functionId,
                   code: newDatapoints[unqCatIndex].code ? newDatapoints[unqCatIndex].code : '',
                   status: true
                 }, { $set: newDatapoints[unqCatIndex] }, { upsert: true })
@@ -4805,9 +4801,13 @@ export const downloadSubsetTaxmonony = async (req, res, next) => {
             obj["Key Issue"] = dataPoints[index]["keyIssueId"] ? dataPoints[index]["keyIssueId"]["keyIssueName"] : "";
             obj["Key Issue Code"] = dataPoints[index]["keyIssueId"] ? dataPoints[index]["keyIssueId"]["keyIssueCode"] : ""
           }
+          if(dataPoints[index]["additionalDetails"].hasOwnProperty(filteredFields[index1]["fieldName"])){
+            //returns true;            
+            obj[filteredFields[index1]["name"]] = dataPoints[index]["additionalDetails"][filteredFields[index1]["fieldName"]];            
+            }
           obj["Is Priority"] = "";
-          obj["Relevant for India"] = "";
-          obj["Polarity"] = "";
+          // obj["Relevant for India"] = dataPoints[index]["relevantForIndia"] ? dataPoints[index]["relevantForIndia"] : "";
+          obj["Polarity"] = dataPoints[index]["polarity"] ? dataPoints[index]["polarity"] : "";
         }
       }
       response.push(obj);
