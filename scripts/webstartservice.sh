f6d63345408ef9f390ea4a8993bf9f1f86d411df
ajaykdiwakar <91321811+ajaykdiwakar@users.noreply.github.com>
2021-11-24 18:27:21 +0530

    add pm2 script in this webstartscript

--- scripts/webstartservice.sh
+++ scripts/webstartservice.sh
@@ -4,3 +4,9 @@ sudo systemctl start web
 sudo systemctl stop web
 sudo systemctl restart web
 sudo systemctl enable web
+
+npm install pm2 -g && pm2 update
+cd /app/esgapi
+pm2 start src/esgapi.js
+echo "pm2 list"
+
