0a52ee57e6d754355776c0a1856fbeb91697a0e7
ajaykdiwakar <91321811+ajaykdiwakar@users.noreply.github.com>
2021-12-01 22:37:56 +0530

    added webstop file wrote on unix sys

---
+++ scripts/webstop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+STATUS="$(systemctl is-active web.service)"
+if [ "${STATUS}" = "active" ]; then
+   sudo systemctl stop web
+   echo " Service web is stopped successfully....."
+else
+   echo " Service failed to stop"
+fi
