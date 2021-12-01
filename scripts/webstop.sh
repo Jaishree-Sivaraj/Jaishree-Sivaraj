9f331fc045a0bc0ac6c146afd522eb909dae2708
ajaykdiwakar <91321811+ajaykdiwakar@users.noreply.github.com>
2021-12-01 21:25:47 +0530

    Update webstop.sh

--- scripts/webstop.sh
+++ scripts/webstop.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
 if [ "$DEPLOYMENT_GROUP_NAME" == "ESGDS-Backend-prod-DeploymentGroup" ]; then
-	STATUS="$(systemctl is-active web.service)"
-	if [ "${STATUS}" = "active" ]; then
-	   sudo systemctl stop web
-	   echo " Service web is stopped successfully....."
-	else
-	   echo " Service failed to stop"
-	fi
+	 STATUS="$(systemctl is-active web.service)"
+	 if [ "${STATUS}" = "active" ]; then
+	    sudo systemctl stop web
+	    echo " Service web is stopped successfully....."
+	 else
+	    echo " Service failed to stop"
+	 fi
 else 
-  cd /app/esgapi
-	pm2 stop 0
+   cd /app/esgapi
+	 pm2 stop 0
 fi
