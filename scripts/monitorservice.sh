2c8a11eeb4cba811ec2e9d2c37a711cb6f6c997d
ajaykdiwakar <91321811+ajaykdiwakar@users.noreply.github.com>
2021-12-01 21:08:42 +0530

    Update monitorservice.sh

--- scripts/monitorservice.sh
+++ scripts/monitorservice.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-#Starting the services ( Node service )
+#Checking if services started and running ( Node service or pm2 service )
 
-cd /app/esgapi
-STATUS="$(systemctl is-active web.service)"
-if [ "${STATUS}" = "active" ]; then
-   echo " Success! Node is running ....."
-else
-   echo " Node Service is not running.... so exiting "
-   exit 1
-fi
 
+if [ "$DEPLOYMENT_GROUP_NAME" == "ESGDS-Backend-prod-DeploymentGroup" ]; then
+	cd /app/esgapi
+	STATUS="$(systemctl is-active web.service)"
+	if [ "${STATUS}" = "active" ]; then
+	   echo " Success! Node is running ....."
+	else
+	   echo " Node Service is not running.... so exiting "
+	   exit 1
+	fi
 
 #zipping the folder after successfull deployment and build
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "ESGDS-Backend-prod-DeploymentGroup" ]; then
-   cd /app
-   rm -rf esgapi-$(date +"%Y-%m-%d")_old.zip
-   mv esgapi-$(date +"%Y-%m-%d").zip esgapi-$(date +"%Y-%m-%d")_old.zip
-   #rm -rf esgapi.zip
-   zip -r -X esgapi-$(date +"%Y-%m-%d").zip esgapi
+	cd /app
+	rm -rf esgapi-$(date +"%Y-%m-%d")_old.zip
+	mv esgapi-$(date +"%Y-%m-%d").zip esgapi-$(date +"%Y-%m-%d")_old.zip
+	#rm -rf esgapi.zip
+	zip -r -X esgapi-$(date +"%Y-%m-%d").zip esgapi
 
-   ##upload to s3. AWS CLI must be installed
+#Zipping esgapi folder and starting upload process to s3 post successfull deployment of application. AWS CLI must be installed
 
-   sudo aws s3 cp esgapi-$(date +"%Y-%m-%d").zip s3://esg-codedeploy-revisions/esgapi-$(date +"%Y-%m-%d").zip
+	sudo aws s3 cp esgapi-$(date +"%Y-%m-%d").zip s3://esg-codedeploy-revisions/esgapi-$(date +"%Y-%m-%d").zip
+
+else
+    echo " Its not prod environment , all done "
+
 fi
