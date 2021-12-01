94bd8de1b8daab146370250d478c0bf7ab0ede62
ajaykdiwakar <91321811+ajaykdiwakar@users.noreply.github.com>
2021-12-01 21:18:31 +0530

    Update webstop.sh

--- scripts/webstop.sh
+++ scripts/webstop.sh
@@ -9,6 +9,6 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "ESGDS-Backend-prod-DeploymentGroup" ]; then
 	   echo " Service failed to stop"
 	fi
 else 
-    cd /app/esgapi
+  cd /app/esgapi
 	pm2 stop src/esgapi.js
 fi
