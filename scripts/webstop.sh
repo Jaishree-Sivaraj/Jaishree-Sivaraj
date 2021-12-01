752b590ff44b19dd7bf2171a2942fc3cfa5413fe
ajaykdiwakar <91321811+ajaykdiwakar@users.noreply.github.com>
2021-12-01 21:35:48 +0530

    Add files via upload

---
+++ scripts/webstop.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+if [ "$DEPLOYMENT_GROUP_NAME" == "ESGDS-Backend-prod-DeploymentGroup" ]; then
+	STATUS="$(systemctl is-active web.service)"
+	if [ "${STATUS}" = "active" ]; then
+	   sudo systemctl stop web
+	   echo " Service web is stopped successfully....."
+	else
+	   echo " Service failed to stop"
+	fi
+else 
+    cd /app/esgapi
+	pm2 stop 0
+fi
