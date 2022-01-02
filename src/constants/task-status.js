9f07534fc40e052478d2c9359b54f8a8f1e838a3
pemadechen209 <padenlaa@gmail.com>
2022-01-02 13:21:20 +0530

    added constant values.

--- src/constants/task-status.js
+++ src/constants/task-status.js
@@ -4,3 +4,4 @@ export const ReassignmentPending='Reassignment Pending';
 export const CorrectionCompleted='Correction Completed';
 export const Completed='Completed';
 export const CollectionCompleted='Collection Completed';
+export const Pending ='Pending';
