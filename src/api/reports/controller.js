bfac92ce4656b062e2dcb987bca5c969abd5a4cc
Naveen Mannem <naveen.m@indiumsoft.com>
2022-04-12 10:17:02 +0530

    Mapping the value of collectionYear to Year field in the Export reports api

--- src/api/reports/controller.js
+++ src/api/reports/controller.js
@@ -382,7 +382,7 @@ export const exportReport = async (req, res, next) => {
             let childDpDetails = allChildDpDetails.filter((obj) =>
               obj.parentDpId == stdData?.datapointId?.id && obj?.companyId == stdData?.companyId?.id && obj?.year == stdData?.year
             )
-            let yearVal = stdData?.year.split('-');
+            let yearVal = stdData?.additionalDetails?.collectionYear.split('-');
             cltTaxoDetails.push(clientTaxonomyDetail.outputFields['cin']);
             cltTaxoDetails.push(clientTaxonomyDetail.outputFields['companyName']);
             cltTaxoDetails.push(clientTaxonomyDetail.outputFields['nicIndustry']);
